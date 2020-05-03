0969ddcb924baa620fbfcf38588c15f79b722cbb
vinzdeveloper <vinzdeveloper@gmail.com>
2020-05-03 22:09:24 +0100

    testing pre hook commit

--- src/components/navigation/navigation-panel.js
+++ src/components/navigation/navigation-panel.js
@@ -6,7 +6,7 @@ import FooterLinks from '../footer/footer';
 import footerLinks from '../../data-objects/footer-links.json';
 
 const navmenu = [{ name: 'Create Rules', navigate: './create-ruleset', iconClass: "icon fa fa-plus-square-o", linkClass: 'navmenu'},
-{ name: 'Upload Rules', navigate: './home', iconClass: "icon fa fa-cloud-upload", linkClass: 'navmenu' } ];
+                 { name: 'Upload Rules', navigate: './home', iconClass: "icon fa fa-cloud-upload", linkClass: 'navmenu' } ];
 class NavigationPanel extends Component {
 
     constructor(props) {
@@ -31,7 +31,7 @@ class NavigationPanel extends Component {
     render() {
         const { closedState, loggedIn } = this.props;
         let rulesetLink = this.props.rulenames.length > 0 ?
-         [{name: 'Ruleset', sublinks: this.props.rulenames, iconClass:"rules-icon", linkClass: 'link-heading'}] : [];
+         [{ name: 'Ruleset', sublinks: this.props.rulenames, iconClass:"rules-icon", linkClass: 'link-heading'}] : [];
 
         rulesetLink = rulesetLink.concat(navmenu);
 
--- src/components/notification/notification.js
+++ src/components/notification/notification.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, memo } from 'react';
 import PropTypes from 'prop-types';
 import 'react-notifications/lib/notifications.css';
 import {NotificationContainer, NotificationManager} from 'react-notifications';
 
 const Notification = (props) => {
 
+    console.log("testing pre hook commit");
     useEffect(() => {
         NotificationManager[props.type](props.body, props.heading, 5000);
     })
@@ -25,4 +26,4 @@ Notification.propTypes = {
     type: PropTypes.string,
 };
 
-export default Notification;
+export default memo(Notification);

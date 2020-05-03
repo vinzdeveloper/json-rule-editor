57239aac1d51465ed2bca71108b190dea4c7b3f7
vinzdeveloper <vinzdeveloper@gmail.com>
2020-05-03 23:04:00 +0100

    testing pre hook commit

--- src/components/notification/notification.js
+++ src/components/notification/notification.js
@@ -1,12 +1,10 @@
 import React, { useEffect, memo } from 'react';
 import PropTypes from 'prop-types';
 import 'react-notifications/lib/notifications.css';
-import {NotificationContainer, NotificationManager} from 'react-notifications';
+import { NotificationContainer, NotificationManager } from 'react-notifications';
 
 const Notification = (props) => {
 
-    console.log("testing pre hook commit");
-    console.log("testing pre hook commit2");
     useEffect(() => {
         NotificationManager[props.type](props.body, props.heading, 5000);
     })

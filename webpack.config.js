a71de848f2ebacb27fc71bdd2eb9afe9388e8903
avinashbn2 <avinash.naik@lendfoundry.com>
2021-02-27 20:53:03 +0530

    fix env

--- webpack.config.js
+++ webpack.config.js
@@ -1,76 +1,100 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
+const webpack = require('webpack');
+
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer-sunburst').BundleAnalyzerPlugin;
 
+module.exports = (env, arg) => {
+	return {
+		entry: ['./src/app.js', './src/sass/base.scss'],
+		output: {
+			filename: 'main.bundle.js',
+			path: path.resolve(__dirname, 'dist')
+		},
+		devtool: arg.mode != 'production' ? 'eval-source-map' : 'nosources-source-map',
+		module: {
+			rules: [
+				{
+					test: /\.(js|jsx)$/,
+					use: ['babel-loader'],
+					exclude: /node_modules/
+				},
+				{
+					test: /\.(jpe|jpg|png)(\?.*$|$)/,
+					loader: 'file-loader',
+					options: {
+						name: '[path][name].[ext]'
+					}
+				},
+				{
+					test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+					loader: 'url-loader?limit=10000&minetype=application/font-woff',
+					options: {
+						name: '[path][name].[ext]'
+					}
+				},
+				{
+					test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+					loader: 'file-loader',
+					options: {
+						name: '[path][name].[ext]'
+					}
+				},
+				{
+					test: /\.scss$/,
+					use: [
+						{ loader: MiniCssExtractPlugin.loader },
+						{ loader: 'css-loader' },
+						{
+							loader: 'sass-loader',
+							options: {
+								sourceMap: false
+							}
+						}
+					],
+					exclude: /node_modules/
+				},
+				{
+					test: /\.css$/,
+					use: ['style-loader', 'css-loader']
+				}
+			]
+		},
+		resolve: {
+			extensions: ['.ts', '.tsx', '.js', '.jsx'],
+			alias: {
+				'react-dom': '@hot-loader/react-dom'
+			}
+		},
+		plugins: [
+			// plugins: options.plugins.concat([
+			// Always expose NODE_ENV to webpack, in order to use `process.env.NODE_ENV`
+			// inside your code for any environment checks; Terser will automatically
+			// drop any unreachable code.
+			// new webpack.EnvironmentPlugin({
+			// 	NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+			// 	REACT_APP_GITHUB_TOKEN: JSON.stringify(process.env.REACT_APP_GITHUB_TOKEN)
+			// }),
+			new webpack.DefinePlugin({
+				'process.env': {
+					NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+					REACT_APP_GITHUB_TOKEN: JSON.stringify(process.env.REACT_APP_GITHUB_TOKEN)
+				}
+			}),
 
-module.exports = (env, arg) => ({
-    entry: ['./src/app.js','./src/sass/base.scss'],
-    output: {
-      filename: "main.bundle.js",
-      path: path.resolve(__dirname, 'dist')
-    },
-    devtool: arg.mode != 'production' ? 'eval-source-map': 'nosources-source-map',
-    module: {
-        rules: [{ 
-          test: /\.(js|jsx)$/, 
-          use: ['babel-loader'],
-          exclude: /node_modules/,
-        }, {
-          test: /\.(jpe|jpg|png)(\?.*$|$)/,
-          loader: 'file-loader',
-          options: {
-            name: '[path][name].[ext]',
-          }
-        }, 
-        {
-           test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-           loader: "url-loader?limit=10000&minetype=application/font-woff",
-           options: {
-            name: '[path][name].[ext]',
-          }
-        },
-        { 
-          test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
-          loader: "file-loader",
-          options: {
-            name: '[path][name].[ext]',
-          }
-        },
-        {
-          test: /\.scss$/,
-          use: [
-          { loader: MiniCssExtractPlugin.loader }, 
-          { loader: "css-loader" },
-          { loader: "sass-loader",
-            options: {
-              sourceMap: false
-            }
-          }],
-          exclude: /node_modules/,
-        }, {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
-        }
-      ]
-    },
-    resolve: {
-      extensions: ['.ts', '.tsx', '.js', '.jsx'],
-      alias: {
-        'react-dom': '@hot-loader/react-dom',
-      }
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: 'index.html',
-            favicon: "./assets/icons/rule.ico"
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[id].css'
-        }),
-        /* new BundleAnalyzerPlugin({
+			// ]),
+			new HtmlWebPackPlugin({
+				template: 'index.html',
+				favicon: './assets/icons/rule.ico'
+			}),
+			new MiniCssExtractPlugin({
+				filename: '[name].css',
+				chunkFilename: '[id].css'
+			})
+			/* new BundleAnalyzerPlugin({
             analyzerMode: 'static',
         }), */
-    ]
-});
+		]
+	};
+};

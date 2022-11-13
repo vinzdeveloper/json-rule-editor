6e234664f0fd808379cd0ee8bb2e38d2f647b3a7
Luca Bergesio <luca.bergesio@gmail.com>
2022-11-13 02:32:39 +0100

    Configured to use webpack 5. Updated packages to work with python 3.x. Added entries to .gitignore file.

--- webpack.config.js
+++ webpack.config.js
@@ -5,72 +5,83 @@ const path = require('path');
 
 
 module.exports = (env, arg) => ({
-    entry: ['./src/app.js','./src/sass/base.scss'],
-    output: {
-      filename: "main.bundle.js",
-      path: path.resolve(__dirname, 'dist')
-    },
-    devtool: arg.mode != 'production' ? 'eval-source-map': 'nosources-source-map',
-    module: {
-        rules: [{ 
-          test: /\.(js|jsx)$/, 
-          use: ['babel-loader'],
-          exclude: /node_modules/,
-        }, {
-          test: /\.(jpe|jpg|png)(\?.*$|$)/,
-          loader: 'file-loader',
-          options: {
-            name: '[path][name].[ext]',
-          }
-        }, 
-        {
-           test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-           loader: "url-loader?limit=10000&minetype=application/font-woff",
-           options: {
-            name: '[path][name].[ext]',
-          }
-        },
-        { 
-          test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
-          loader: "file-loader",
-          options: {
-            name: '[path][name].[ext]',
-          }
-        },
-        {
-          test: /\.scss$/,
-          use: [
-          { loader: MiniCssExtractPlugin.loader }, 
-          { loader: "css-loader" },
-          { loader: "sass-loader",
-            options: {
-              sourceMap: false
-            }
-          }],
-          exclude: /node_modules/,
-        }, {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
-        }
-      ]
-    },
-    resolve: {
-      extensions: ['.ts', '.tsx', '.js', '.jsx'],
-      alias: {
-        'react-dom': '@hot-loader/react-dom',
+  entry: ['./src/app.js', './src/sass/base.scss'],
+  output: {
+    filename: "main.bundle.js",
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'development',
+  devtool: arg.mode != 'production' ? 'eval-source-map' : 'nosources-source-map',
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      use: ['babel-loader'],
+      exclude: /node_modules/,
+    }, {
+      test: /\.(jpe|jpg|png)(\?.*$|$)/,
+      loader: 'file-loader',
+      options: {
+        name: '[path][name].[ext]',
       }
     },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: 'index.html',
-            favicon: "./assets/icons/rule.ico"
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[id].css'
-        }),
-        /* new BundleAnalyzerPlugin({
-            analyzerMode: 'static',
-        }), */
+    {
+      test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      use: [{
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+          mimetype: 'application/font-woff',
+          name: '[path][name].[ext]'
+        }
+      }]
+      //  loader: "url-loader?limit=10000&mimetype=application/font-woff",
+      // options: {
+      //   name: '[path][name].[ext]',
+      // }
+    },
+    {
+      test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      loader: "file-loader",
+      options: {
+        name: '[path][name].[ext]',
+      }
+    },
+    {
+      test: /\.scss$/,
+      use: [
+        { loader: MiniCssExtractPlugin.loader },
+        { loader: "css-loader" },
+        {
+          loader: "sass-loader",
+          options: {
+            sourceMap: false
+          }
+        }],
+      exclude: /node_modules/,
+    }, {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+    }
     ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    alias: {
+      'react-dom': '@hot-loader/react-dom',
+    },
+    fallback: { "vm": false }
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: 'index.html',
+      favicon: "./assets/icons/rule.ico"
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    }),
+    /* new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+    }), */
+  ]
 });
